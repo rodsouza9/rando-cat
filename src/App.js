9bcace5c35a2b9b8359da8bf4302430417dcd2af
Rohan D'Souza <rodsouza9@gmail.com>
2019-12-02 22:38:43 -0800

    Create Cat and Image classes. Got random cat images to show on refresh.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOM from "react-dom";
+import Request from 'react-http-request';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Cat />
+        <Image src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,5 +27,60 @@ function App() {
     </div>
   );
 }
+
+class Cat extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      url: "ROAN",
+    };
+  }
+  fetchUrl() {
+
+  }
+
+  render () {
+    return(
+      <Request
+        url='https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat'
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            }
+            else if (error) {return <div>eRROR...</div>}
+            else {
+              this.state.url = result.body.data.images.original.url;
+              return (
+                <div>
+                  {JSON.stringify(result.body.data.images.original.url) }
+                  <p>{this.state.url}</p>
+                  <Image src={this.state.url}/>
+                </div>
+              );
+            }
+          }
+        }
+      </Request>
+    );
+  }
+}
+
+class Image extends React.Component {
+  render() {
+    return (
+      <img
+      src={this.props.src}
+      alt="new"
+      />
+    );
+  }
+}
+
+
 
 export default App;
